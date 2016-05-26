d79fd8afe2812cdf8257029d9c530e99a292c617
davluen <davidluengo@msn.com>
2016-05-26 21:14:23 +0200

    Update peon.h

--- peon.h
+++ peon.h
@@ -1,40 +1,105 @@
 //
 // Created by Daniel Sánchez Vicente on 20/5/16.
-//
+// YA FUNCIONA LA ASCENSION
 
-#ifndef AJEDREZ_PEON_H
-#define AJEDREZ_PEON_H
+#include "peon.h"
+#include "casilla.h"
+#include "tablero.h"
+#include "juego.h"
+#include "reina.h"
+#include "rey.h"
+#include "jugador.h"
 
-#include "pieza.h"
+//NOTA: las blancas comienzan en las casillas bajas (0,1)
+//		las negras comienzan en las casillas altas (6,7)
 
-class casilla;
-class tablero;
-class reina;
-class pieza;
-class juego;
-class jugador;
-class rey;
+#define DIREC_B -1
+#define DIREC_N 1
+#define FIN_N 7
+#define FIN_B 0
+
+peon::peon() { }
+
+peon::peon(bool &color) : pieza(color) {
+
+}
+
+peon::~peon() {
+	delete _ascension;
+}
+
+const bool peon::movLegal(casilla &destino) const {
+
+	int direccion = DIREC_B; //Blancas por defecto para ahorrarnos un else
+	bool esLegal = false;
+
+	if (this->color() == 0) {
+		direccion = DIREC_N;
+	}  // Por si la pieza es negra
+
+	if (_ascension) {
+		esLegal = _ascension->movLegal(destino);
+	}
+
+	else if (!_ascension) {
+		if (!destino.ocupada()) {
+			// Está libre el destino?
+			if (destino.coordY() == this->posicion().coordY()) {
+				//está en dirección vertical?
+				if (!pieza::movida()) {
+					//Ha sido movido el peon anteriormente?
+					if (destino.coordX() == (this->posicion().coordX() + 2 * direccion)) {
+						// Si va a mover dos casillas a la vez
+						if (!(tablero::getTablero().casillaPorCoord(this->posicion().coordX() + 1 * direccion, this->posicion().coordY()).ocupada())) {
+							//Este if toma el tablero y, con las coordenadas de la posicion, comprueba si la casilla delantera o trasera está libre
+							esLegal = true;
+						}
+					}
+				}
+				if (destino.coordX() == (this->posicion().coordX() + 1 * direccion)) {
+					esLegal = true;
+				}
+			}
+		}
+
+		else if ((destino.coordX() == (this->posicion().coordX() + 1 * direccion)) && ((destino.coordY() == (this->posicion().coordY() + 1)) || (destino.coordY() == (this->posicion().coordY() - 1)))) {
+			//Movimiento oblicuo: si avanza 1 y se mueve a derecha o izquierda 1
+			if (destino.ocupante().color() != this->color()) {
+				esLegal = true;
+			}
+		}
+	}
 
 
-class peon : public pieza {
-public:
-    peon();
 
-    peon(bool& color);
+	return esLegal;
 
-    ~peon();
+}
 
-    const bool movLegal(casilla& destino) const;
+const bool peon::moverPieza(const jugador &jugadorActual, casilla &destino) {
+	int final = FIN_N;
+	bool col = this->color();
+	if (this->color()) final = FIN_B;
+	bool esLegal = pieza::moverPieza(jugadorActual, destino);
+	if (esLegal) {
+		if (!_ascension&&(posicion().coordX() == final)) {
+			_ascension = new reina(col);
+		//}
+		//if (_ascension) {
+			//Poner la reina donde el peon
+			_ascension->poner(&destino);
+			//Comprobar si la reina hace jaque
+			if (this->movLegal(juego::adversario(jugadorActual).miRey().posicion())) cout << "Jaque" << endl;
 
-    const void imprimir(ostream& salida) const;
-
-    const bool moverPieza(const jugador& jugadorActual, casilla& destino);
-
-
-
-private:
-    reina* _ascension;
+		}
+		juego::resetContadorTablas();
+	}
+	return esLegal;
 };
 
 
-#endif //AJEDREZ_PEON_H
+
+const void peon::imprimir(ostream& salida) const {
+	if(!_ascension) salida << color() << string("P");
+	else salida << color() << string("A");
+}
